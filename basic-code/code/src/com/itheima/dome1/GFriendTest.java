828505dbf77e0c53f519d1167a2c4460ce52035c
beakless <2141425009@qq.com>
2022-11-19 11:20:32 +0800

    object practice

---
+++ basic-code/code/src/com/itheima/dome1/GFriendTest.java
@@ -0,0 +1,13 @@
+package com.itheima.dome1;
+
+public class GFriendTest {
+    public static void main(String[] args) {
+        GFriend gFriend = new GFriend();
+        gFriend.setName("lili");
+        gFriend.setHigh(163.7);
+        gFriend.setWeight(110.9);
+        gFriend.show();
+        gFriend.wash();
+        gFriend.cook();
+    }
+}
